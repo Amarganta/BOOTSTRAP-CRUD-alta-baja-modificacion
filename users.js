508cd561e5095ca4675331046164299023fe8440
Sofia <sofi1494@outlook.es>
2021-01-11 23:25:21 -0300

    [punto-4] - agregue validaciones y corregi comportamiento del formulario al crear un usuario nuevo

--- users.js
+++ users.js
@@ -3,38 +3,38 @@ const urlBase = 'https://tp-3-c1096-default-rtdb.firebaseio.com/';
 const tableUsers = document.getElementById('users-list');
 const createTable = () => {
     fetch(urlBase + '/users.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        const tbody = tableUsers.querySelector('tbody');
-        tbody.innerHTML = '';
-        
-        for(let prop in data) {
-            const user = data[prop];
-            const tr = document.createElement('tr');
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const tbody = tableUsers.querySelector('tbody');
+            tbody.innerHTML = '';
 
-            const checkBox = document.createElement('span');
-            const input = document.createElement('input');
-            input.type = 'checkbox'
-            input.className = 'custom-checkbox';
-            const td2 = document.createElement('td');
-            checkBox.appendChild(input);
-            // input.appendChild(td2);
-            td2.appendChild(checkBox);
-            tr.appendChild(td2);
-            tbody.appendChild(tr)
-            
-            const campos = [user.name, user.email, user.address, user.phone]
-            for(campo of campos) {
+            for (let prop in data) {
+                const user = data[prop];
+                const tr = document.createElement('tr');
 
-                const td = document.createElement('td');
-                td.innerHTML = campo;
-                tr.appendChild(td);
-                
-            }
-            
+                const checkBox = document.createElement('span');
+                const input = document.createElement('input');
+                input.type = 'checkbox'
+                input.className = 'custom-checkbox';
+                const td2 = document.createElement('td');
+                checkBox.appendChild(input);
+                // input.appendChild(td2);
+                td2.appendChild(checkBox);
+                tr.appendChild(td2);
+                tbody.appendChild(tr)
 
-            const botonEliminar = document.createElement('button');
+                const campos = [user.name, user.email, user.address, user.phone]
+                for (campo of campos) {
+
+                    const td = document.createElement('td');
+                    td.innerHTML = campo;
+                    tr.appendChild(td);
+
+                }
+
+
+                const botonEliminar = document.createElement('button');
                 botonEliminar.className = 'btn btn-danger';
                 botonEliminar.innerHTML = `<i class="fa fa-trash" aria-hidden="true"></i>`;
                 const td = document.createElement('td');
@@ -43,10 +43,10 @@ const createTable = () => {
                 td.appendChild(botonEliminar);
                 tr.appendChild(td);
                 tbody.appendChild(tr);
-            
-        }
-    })            
-    .catch((error => console.log(error)))
+
+            }
+        })
+        .catch((error => console.log(error)))
 
 }
 const deleteUser = (id) => {
@@ -55,5 +55,28 @@ const deleteUser = (id) => {
     }).then(() => createTable())
 
     console.log('Eliminar' + id)
+}
+const validateName = () => {
+    let name = document.getElementById("add-name").value
+    return name.length <= 50
+}
+
+const validateAddress = () => {
+    let address = document.getElementById("add-address").value
+    return address.length <= 60
+}
+
+
+const validateEmail = () => {
+    let email = document.getElementById("add-email").value
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+    return emailRegex.test(email)
+
+}
+const validateNumber = () => {
+    let number = document.getElementById("add-phone").value
+    numberRegex = /[0-9-]+$/i;
+    return numberRegex.test(number)
 }
 createTable();
